5a4fc356ed1a6586e842d921efda7c442873be6b
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-12-03 14:53:06 +0000

    SDIT-2337: ✨ Add in initial detail page (#24)

---
+++ integration_tests/e2e/detail.cy.ts
@@ -0,0 +1,20 @@
+import Page from '../pages/page'
+import Disclaimer from '../pages/disclaimer'
+import DetailPage from '../pages/detail'
+
+context('Detail', () => {
+  beforeEach(() => {
+    cy.task('reset')
+    cy.task('stubFrontendComponents')
+    cy.task('stubSignIn', { roles: ['ROLE_HPA_USER'] })
+    cy.signIn()
+    Page.verifyOnPage(Disclaimer).confirmDisclaimer()
+  })
+
+  it('Will show prisoner detail', () => {
+    cy.task('stubPrisonerDetail')
+
+    cy.visit('/detail/A1234BC')
+    Page.verifyOnPageWithTitleParam(DetailPage, 'Firsta Middlea SURNAMEA')
+  })
+})
