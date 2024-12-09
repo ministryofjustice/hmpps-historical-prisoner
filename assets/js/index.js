341018e95b962c7a4f9283dc85d0fab27c0d3967
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-12-09 11:06:47 +0000

    SDIT-2337: ✨ Add in back to top link (#42)

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,16 @@
 import * as govukFrontend from 'govuk-frontend'
 import * as mojFrontend from '@ministryofjustice/frontend'
+import setupBackToTop from './backToTop'
 
 govukFrontend.initAll()
 mojFrontend.initAll()
+
+document.addEventListener('DOMContentLoaded', () => {
+  const body = document.querySelector('body')
+  const pageType = body.getAttribute('data-page')
+  switch (pageType) {
+    case 'detail':
+      setupBackToTop()
+      break
+  }
+})
