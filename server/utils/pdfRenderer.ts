39ba7845da809be530944fcfd5c4b5b3c492cb20
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2024-12-12 21:39:13 +0000

    SDIT-2353: ✨ More work on the download

--- server/utils/pdfRenderer.ts
+++ server/utils/pdfRenderer.ts
@@ -1,4 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
+import path from 'path'
+import fs from 'fs'
+
 import GotenbergClient, { PdfMargins } from '../data/gotenbergClient'
 import logger from '../../logger'
 
@@ -7,6 +10,23 @@ export type PdfHeaderData = Record<string, unknown>
 export type PdfFooterData = Record<string, unknown>
 
 export function pdfRenderer(client: GotenbergClient) {
+  function readAssetCss() {
+    try {
+      // have to go to the manifest to translate to the real path of the css file e.g. /assets/css/app.ESCUP4HF.css
+      const assetMetadataPath = path.resolve(__dirname, '../../assets/manifest.json')
+      const assetManifest: Record<string, string> = JSON.parse(fs.readFileSync(assetMetadataPath, 'utf8'))
+      const cssPath = '/assets/css/app.css'
+      const realCssPath = assetManifest[cssPath] || cssPath
+      // and then read the file. This saves gotenberg then having to fetch the css file from the node instance
+      return fs.readFileSync(path.resolve(__dirname, `../../${realCssPath}`), 'utf8')
+    } catch (e) {
+      if (process.env.NODE_ENV !== 'test') {
+        logger.error(e, 'Could not read asset css file')
+      }
+      return null
+    }
+  }
+
   return (_req: Request, res: Response, next: NextFunction) => {
     res.renderPdf = (
       pageView: string,
@@ -30,15 +50,16 @@ export function pdfRenderer(client: GotenbergClient) {
               throw bodyError
             }
 
-            res.header('Content-Type', 'application/pdf')
-            res.header('Content-Transfer-Encoding', 'binary')
-            res.header('Content-Disposition', `inline; filename=${options.filename}`)
-
             client
-              .renderPdfFromHtml(pageHtml, headerHtml, footerHtml, options?.pdfMargins)
-              .then(buffer => res.send(buffer))
-              .catch(reason => {
-                logger.warn(reason)
+              .renderPdfFromHtml(pageHtml, headerHtml, footerHtml, readAssetCss(), options?.pdfMargins)
+              .then(buffer => {
+                res.header('Content-Type', 'application/pdf')
+                res.header('Content-Transfer-Encoding', 'binary')
+                res.header('Content-Disposition', `attachment; filename=${options.filename}`)
+                res.send(buffer)
+              })
+              .catch(error => {
+                next(error)
               })
           })
         })
