0184d39f17c5df35a7b4c585f4dce3ea69176ca0
karenmillermoj <karen.miller@digital.justice.gov.uk>
2025-01-03 17:37:33 +0000

    SDIT-2381 Add shortlist comparison tests

--- integration_tests/pages/comparison.ts
+++ integration_tests/pages/comparison.ts
@@ -1,7 +1,13 @@
-import Page from './page'
+import Page, { PageElement } from './page'
 
 export default class Comparison extends Page {
   constructor() {
     super('Prisoner comparison')
   }
+
+  detailLink = (index: number): PageElement => cy.get(`[data-qa="name${index}"]`)
+
+  removeFromShortlist = (index: number): PageElement => cy.get(`[data-qa="remove-link${index}"]`)
+
+  backLink = (): PageElement => cy.get('.govuk-back-link')
 }
